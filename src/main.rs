32331dd1bf1814df7de8821d44ad8f3919036252
Michal Babinski <mbabins5@uoregon.edu>
2022-02-14 19:43:00 -0700

    rust run mods

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+// mod vars;
+
+// mod types;
+
+// mod strings;
+
+// mod tuples;
+
+// mod arrays;
+
+// mod vectors;
+
+// mod conditionals;
+
+// mod functions;
+
+// mod pointers;
+
+// mod structs;
+
+// mod enums;
+
+mod cli;
+
+fn main() {
+    cli::run();
+}
